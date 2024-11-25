efa1d00030013b2adad4ee065f8cfa834973113f
Thomas W. C. Carlson <72635603+TWCCarlson@users.noreply.github.com>
2024-11-24 16:29:15 -0800

    Basic structure
    Also testing storing json data on Github

--- streamlit_main.py
+++ streamlit_main.py
@@ -14,5 +14,16 @@ BRACKETS_SINGLE_FILER = {
     np.inf: 0.37
 }
 
+st.selectbox("Select your country:", ["United States"])
+st.selectbox("Select the fiscal year:", [2025], help="""Each year has different
+             tax brackets.""")
+st.selectbox("Choose your filing status:",
+             ["Single Filer", "Head of Household", "Married Filing Separately",
+              "Married Filing Jointly"], help="""Generally, tax brackets 
+              favor those with dependents.""")
+st.number_input(label="Input your taxable income (in your country's currency):",
+                format="%.2f", step=1000.00, min_value=0.00, value=65000.00)
+
+
 chart = create_bracket_graph.TaxBracketBreakdownGraph(INCOME, BRACKETS_SINGLE_FILER)
 st.altair_chart(chart.get_full_combochart(), theme=None, use_container_width=True)
